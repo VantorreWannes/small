4c8f8050cd075d889fd379853002408912dcca2a
Navoso <VantorreWannes@gmail.com>
2024-02-15 21:32:57 +0100

    Implement ToBitStream for SmlHeader

--- src/header.rs
+++ src/header.rs
@@ -1,3 +1,5 @@
+use bitstream_io::ToBitStream;
+
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub struct SmlHeader {
     bool: u8,
@@ -48,5 +50,34 @@ impl SmlHeader {
             f32,
             f64,
         }
+    }
+}
+
+impl ToBitStream for SmlHeader {
+    type Error = std::io::Error;
+
+    fn to_writer<W: bitstream_io::BitWrite + ?Sized>(
+        &self,
+        writer: &mut W,
+    ) -> Result<(), Self::Error>
+    where
+        Self: Sized,
+    {
+        const BITS: u32 = u8::BITS - 1;
+        writer.write(BITS, self.bool)?;
+        writer.write(BITS, self.char)?;
+        writer.write(BITS, self.i8)?;
+        writer.write(BITS, self.i16)?;
+        writer.write(BITS, self.i32)?;
+        writer.write(BITS, self.i64)?;
+        writer.write(BITS, self.i128)?;
+        writer.write(BITS, self.u8)?;
+        writer.write(BITS, self.u16)?;
+        writer.write(BITS, self.u32)?;
+        writer.write(BITS, self.u64)?;
+        writer.write(BITS, self.u128)?;
+        writer.write(BITS, self.f32)?;
+        writer.write(BITS, self.f64)?;
+        Ok(())
     }
 }
